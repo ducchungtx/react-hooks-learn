69dc4463aebd75e0f2208c8fb7b5ca34b8b48d75
Chung Nguyen <chungnguyen@Chungs-iMac.local>
2019-04-03 21:21:01 +0700

    Làm việc với object sử dụng useState hook

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleIncrease = () => setCount(count + 1);
-  const handleDecrease= () => setCount(count - 1);
-  const handleReset = () => setCount(0);
+  const [ state, setState ] = useState({
+    city: '',
+    country: ''
+  });
+
+  const handleCityChange = (e) => setState({ ...state, city: e.target.value });
+  const handleCountryChange = (e) => setState({ ...state, country: e.target.value });
+
   return(
-    <div>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-      <button onClick={handleReset}>Reset</button>
-      <h1>{count}</h1>
-    </div>
+    <form>
+      <div>
+        <input
+          type="text"
+          placeholder="City"
+          value={state.city}
+          onChange={handleCityChange}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Country"
+          value={state.country}
+          onChange={handleCountryChange}
+        />
+      </div>
+      <div>
+        You live in { `${state.city}, ${state.country}` }
+      </div>
+    </form>
   )
 }
 
