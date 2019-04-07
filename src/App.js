835fb61603a4ff64e99284a3d4ffa121f475345c
Chung Nguyen <chungnguyen@Chungs-iMac.local>
2019-04-07 17:28:52 +0700

    Giới hạn useEffect mounting and unmounting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Counter() {
   const [count, setCount] = useState(0);
   const handleIncrease = () => setCount(count + 1);
   const handleDecrease= () => setCount(count - 1);
   const handleReset = () => setCount(0);
 
   useEffect(() => {
-    console.log(`I'm in useEffect. The current count is ${count}`);
+    console.log(`I'm in useEffect. I will only run upon mounting. The current count is ${count}`);
     return () => {
-      console.log(`I'm removing anything that was setup above! The last count was ${count}`)
+      console.log(`I'm removing anything that was setup above! The last count was ${count}. But now, I will onlu run when component is being unmounted`)
     }
-  })
+  }, [])
 
   return(
     <div>
@@ -20,6 +20,17 @@ function App() {
       <button onClick={handleDecrease}>Decrease</button>
       <button onClick={handleReset}>Reset</button>
       <h1>{count}</h1>
+    </div>
+  )
+}
+
+function App() {
+  const [ visible, setVisible ] = useState(false)
+
+  return(
+    <div>
+      <button onClick={() => setVisible(!visible)}>Show / Hide Counter component</button>
+      { visible && <Counter /> }
     </div>
   )
 }
