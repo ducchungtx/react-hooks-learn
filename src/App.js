cdd8646e2beef5fb24fbb3d22d7209c0d1f1aecb
Chung Nguyen <chungnguyen@Chungs-iMac.local>
2019-04-07 17:55:21 +0700

    Second agrument useEffect Hook

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import './App.css';
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('salmon');
   const handleIncrease = () => setCount(count + 1);
   const handleDecrease= () => setCount(count - 1);
   const handleReset = () => setCount(0);
+  const handleColorChange = () => {
+    const nextColor = color === 'salmon' ? 'gold' : 'salmon'
+    setColor(nextColor)
+  };
 
   useEffect(() => {
-    console.log(`I'm in useEffect. I will only run upon mounting. The current count is ${count}`);
+    console.log(`I'm in useEffect. The current count is ${count}`);
     return () => {
-      console.log(`I'm removing anything that was setup above! The last count was ${count}. But now, I will onlu run when component is being unmounted`)
+      console.log(`I'm removing anything that was setup above! The last count was ${count}`)
     }
-  }, [])
+  }, [count])
 
   return(
     <div>
       <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleColorChange}>ChangeColor</button>
       <button onClick={handleDecrease}>Decrease</button>
       <button onClick={handleReset}>Reset</button>
-      <h1>{count}</h1>
+      <h1 style={{ color }}>{count}</h1>
     </div>
   )
 }
