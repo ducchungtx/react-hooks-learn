e2f4c1707a455c811b283af01d7401fa927da190
Chung Nguyen <chungnguyen@Chungs-iMac.local>
2019-04-08 21:26:10 +0700

    useEffect User Input

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState('salmon');
-  const handleIncrease = () => setCount(count + 1);
-  const handleDecrease= () => setCount(count - 1);
-  const handleReset = () => setCount(0);
-  const handleColorChange = () => {
-    const nextColor = color === 'salmon' ? 'gold' : 'salmon'
-    setColor(nextColor)
-  };
-
-  useEffect(() => {
-    console.log(`I'm in useEffect. The current count is ${count}`);
-    return () => {
-      console.log(`I'm removing anything that was setup above! The last count was ${count}`)
+function App() { 
+  const [userText, setUserText] = useState('');
+  function handUserKeyPress(event) {
+    const { key, keyCode } = event;
+    if(keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+      setUserText(`${userText}${key}`)
     }
-  }, [count])
+  }
+  useEffect(() => {
+    window.addEventListener('keydown', handUserKeyPress)
+    return () => {
+      window.removeEventListener('keydown', handUserKeyPress)
+    }
+  })
 
   return(
     <div>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleColorChange}>ChangeColor</button>
-      <button onClick={handleDecrease}>Decrease</button>
-      <button onClick={handleReset}>Reset</button>
-      <h1 style={{ color }}>{count}</h1>
-    </div>
-  )
-}
-
-function App() {
-  const [ visible, setVisible ] = useState(false)
-
-  return(
-    <div>
-      <button onClick={() => setVisible(!visible)}>Show / Hide Counter component</button>
-      { visible && <Counter /> }
+      <h1>Feel free to type! Your text will show up below!</h1>
+      <blockquote>
+        { userText }
+      </blockquote>
     </div>
   )
 }
